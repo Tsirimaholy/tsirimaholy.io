df550c129f7e51f2be5c1a19e9f6e93b567f3b9f
Tsirimaholy <hei.tsirimaholy@gmail.com>
2025-05-01 21:56:09 +0300

    Add Vercel Speed Insights

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
   ScrollRestoration,
 } from "react-router";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -34,6 +36,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <Analytics />
+        <SpeedInsights />
         {children}
         <ScrollRestoration />
         <Scripts />
