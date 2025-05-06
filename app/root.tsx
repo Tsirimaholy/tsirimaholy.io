031625e15417e75d197bc26547da11e7a621ddc3
Tsirimaholy <hei.tsirimaholy@gmail.com>
2025-05-06 12:48:03 +0300

    make scroll smooth

--- app/root.tsx
+++ app/root.tsx
@@ -34,7 +34,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="scroll-smooth">
         <Analytics />
         <SpeedInsights />
         {children}
